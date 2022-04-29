d27ba8348961e1760e8b4e09d663b67a25edf36b
AliFarajzade <alifarajzade1@yahoo.com>
2022-04-29 02:49:56 -0400

    Add end-point for creating users.

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
+import * as authControllers from '../controllers/auth.controller'
 
 const router = Router()
 
+router.post('/signup', authControllers.signUpUser)
 
 // prettier-ignore
 router
@@ -10,6 +12,5 @@ router
 // prettier-ignore
 router
     .route('/:id')
-
 
 export default router
