99b6e38b8460c64dddb7c88db4282552c1e7e886
AliFarajzade <alifarajzade1@yahoo.com>
2022-04-29 02:48:15 -0400

    Fix regex.

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,3 +1,3 @@
-export const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/g
+export const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
 export const passwordRegex =
-    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/g
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
