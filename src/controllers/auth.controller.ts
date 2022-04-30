42f744949cf2879fdc7e4635cc2fe331e8729968
AliFarajzade <alifarajzade1@yahoo.com>
2022-04-30 06:40:57 -0400

    Fix signup user bug.

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,7 +5,14 @@ import { catchAsync } from './error.controller'
 export const signUpUser = catchAsync(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async (req: Request, res: Response, _: NextFunction) => {
-        const newUser = await UserModel.create(req.body)
+        const data = {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            confirmPassword: req.body.confirmPassword,
+        }
+
+        const newUser = await UserModel.create(data)
 
         res.status(201).json({
             status: 'success',
