14544767be9371b226c8b94b32ca88e53e66a5dd
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-01 02:49:39 -0400

    Add token to sign up auth.

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
 import UserModel from '../models/user.model'
 import { catchAsync } from './error.controller'
 
@@ -14,8 +15,17 @@ export const signUpUser = catchAsync(
 
         const newUser = await UserModel.create(data)
 
+        const token = jwt.sign(
+            { id: newUser._id },
+            process.env.JWT_SECRET as string,
+            {
+                expiresIn: process.env.JWT_EXPIRE_TIME as string,
+            }
+        )
+
         res.status(201).json({
             status: 'success',
+            token,
             data: {
                 user: newUser,
             },
