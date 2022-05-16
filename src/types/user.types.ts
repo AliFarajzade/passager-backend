4e93ed54595ef213ec4370cad6f10a026d7db541
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-16 06:37:44 -0400

    Add new types for user.

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,10 +1,11 @@
 export type TUser = {
     name: string
     email: string
-    photo: string
+    photo?: string
     password: string
     confirmPassword?: string
     passwordChangedAt?: Date
+    role: 'user' | 'guide' | 'lead-guide' | 'admin'
 }
 
 export type TJWTDecodedType = { id: string; iat: number; exp: number }
