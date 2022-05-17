8cbb18404713030e41ba47e847eb151dd70b67e4
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-17 03:24:24 -0400

    Fix the TControllerCRUDFunction type.

--- src/types/controllers.types.ts
+++ src/types/controllers.types.ts
@@ -1,6 +1,7 @@
-import type { Request, Response, NextFunction } from 'express'
+import type { NextFunction, Request, Response } from 'express'
+import { TUser } from './user.types'
 export type TControllerCRUDFunction = (
-    req: Request,
+    req: Request & Partial<{ currentUser: TUser }>,
     res: Response,
     next: NextFunction
 ) => Promise<void>
