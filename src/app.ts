f88554348705fc66761f35319f9e0cbdb47c32d8
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-22 04:51:59 -0400

    Adding rate limiter functions to users end-point.

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,19 @@ import { errorMiddleware } from './controllers/error.controller'
 import toursRouter from './routes/tours.router'
 import usersRouter from './routes/users.router'
 import AppError from './utils/app-error.class'
+import { customRateLimiter } from './utils/limiter.helper'
 
 const app = express()
 
 // Genral middlewares
+app.use(
+    '/api',
+    customRateLimiter(
+        100,
+        15 * 16 * 1000,
+        'Too many requests! Please try again later.'
+    )
+)
 app.use(express.json())
 process.env.NODE_ENV === 'development' && app.use(morgan('dev'))
 
--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,22 +1,49 @@
 import { Router } from 'express'
 import * as authControllers from '../controllers/auth.controller'
 import * as usersContoller from '../controllers/users.controller'
+import { customRateLimiter } from '../utils/limiter.helper'
 
 const router = Router()
 
-router.post('/register', authControllers.signUpUser)
-router.post('/enter', authControllers.logInUser)
+router.post(
+    '/register',
+    customRateLimiter(5, 60 * 30 * 1000, 'Try making new account later.'),
+    authControllers.signUpUser
+)
+router.post(
+    '/enter',
+    customRateLimiter(7, 60 * 30 * 1000, 'Try loging in later.'),
+    authControllers.logInUser
+)
 
-router.post('/forgot', authControllers.forgotPassword)
-router.patch('/reset/:token', authControllers.resetPassword)
+router.post(
+    '/forgot',
+    customRateLimiter(5, 60 * 30 * 1000, 'Try again later.'),
+    authControllers.forgotPassword
+)
+router.patch(
+    '/reset/:token',
+    customRateLimiter(5, 60 * 30 * 1000, 'Try again later.'),
+    authControllers.resetPassword
+)
 router.patch(
     '/change-password',
+    customRateLimiter(
+        5,
+        60 * 30 * 1000,
+        'Try changing your password in an hour.'
+    ),
     authControllers.protectRoute,
     authControllers.updatePassword
 )
 
 router.patch(
     '/update',
+    customRateLimiter(
+        25,
+        60 * 30 * 1000,
+        'Try updating your profile in an hour.'
+    ),
     authControllers.protectRoute,
     authControllers.updateProfile
 )
