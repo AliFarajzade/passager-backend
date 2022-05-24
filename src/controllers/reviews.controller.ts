b2c3b594e022e84bb9295ac852b46f47c6e0b22b
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-24 06:17:18 -0400

    Create controllers for getting all reviews and creating reviews.

---
+++ src/controllers/reviews.controller.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from 'express'
+import ReviewModel from '../models/review.model'
+import { catchAsync } from './error.controller'
+
+export const getAllReviews = catchAsync(
+    async (_req: Request, res: Response, _: NextFunction) => {
+        // 1) Get all review from database.
+        const reviews = await ReviewModel.find()
+
+        // 2) Send review back to the clinet.
+        res.status(200).json({
+            status: 'success',
+            results: reviews.length,
+            data: {
+                reviews,
+            },
+        })
+    }
+)
+
+export const createNewReview = catchAsync(
+    async (req: Request, res: Response, _: NextFunction) => {
+        // 1)  Create new tour in database.
+        const newReview = await ReviewModel.create(req.body)
+
+        // 2) Send the new review back to the clinet.
+        res.status(200).json({
+            status: 'success',
+            data: {
+                review: newReview,
+            },
+        })
+    }
+)
