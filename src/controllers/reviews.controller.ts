3cff6d25a54b8e2d7cf99a9335399149745ad9b5
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-26 10:27:32 -0400

    Replace getting all reviews function with new factory function.

--- src/controllers/reviews.controller.ts
+++ src/controllers/reviews.controller.ts
@@ -1,33 +1,15 @@
 import type { NextFunction, Request, Response } from 'express'
 import ReviewModel from '../models/review.model'
 import { TUser } from '../types/user.types'
-import { catchAsync } from './error.controller'
 import {
     createDocument,
     deleteDocument,
+    getAllDocuments,
     getDocument,
     updateDocument,
 } from './factory.controller'
 
-export const getAllReviews = catchAsync(async (req, res, _next) => {
-    let filter: Record<string, string> = {}
-
-    if (req.params.id) filter = { tour: req.params.id }
-
-    console.log(filter)
-
-    // 1) Get all review from database.
-    const reviews = await ReviewModel.find(filter)
-
-    // 2) Send review back to the clinet.
-    res.status(200).json({
-        status: 'success',
-        results: reviews.length,
-        data: {
-            reviews,
-        },
-    })
-})
+export const getAllReviews = getAllDocuments(ReviewModel, 'Review')
 
 export const includeReviewFields = (
     req: Request & Partial<{ currentUser: TUser }>,
