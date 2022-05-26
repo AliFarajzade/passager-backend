48a4b0cff256e20cef8bdf6dfaa9967f705390d0
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-26 04:39:58 -0400

    Add function for updating documents.

--- src/controllers/factory.controller.ts
+++ src/controllers/factory.controller.ts
@@ -3,6 +3,16 @@ import { Model } from 'mongoose'
 import AppError from '../utils/app-error.class'
 import { catchAsync } from './error.controller'
 
+const excludeFields = (obj: Record<string, any>, ...fileds: string[]) => {
+    const filteredObj: Record<string, any> = { ...obj }
+
+    Object.keys(obj).forEach(key => {
+        if (fileds.includes(key)) delete filteredObj[key]
+    })
+
+    return filteredObj
+}
+
 export const deleteDocument = (
     Model: Model<
         any,
@@ -21,5 +31,36 @@ export const deleteDocument = (
         res.status(202).json({
             status: 'deleted',
             data: {},
+        })
+    })
+
+export const updateDocument = (
+    Model: Model<
+        any,
+        Record<string, unknown>,
+        Record<string, unknown>,
+        Record<string, unknown>
+    >,
+    modelName = ''
+) =>
+    catchAsync(async (req, res, next) => {
+        const { id } = req.params
+
+        if (modelName === 'Review')
+            req.body = excludeFields(req.body, 'user', 'tour')
+
+        const updatedDocument = await Model.findByIdAndUpdate(id, req.body, {
+            new: true,
+            runValidators: true,
+        })
+
+        if (!updatedDocument)
+            return next(new AppError('No document found with this ID.', 404))
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour: updatedDocument,
+            },
         })
     })
